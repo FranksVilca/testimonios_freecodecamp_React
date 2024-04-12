b1fe510c7ce1e8695ca40c03e66ff40ebcee9a32
Franks Vilca <franksjaviervilcaquispe@gmail.com>
2024-04-11 23:40:31 -0500

    Importado Testimonio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Testimonio from './Componentes'
 function App() {
   return (
     <div className="App">
-     
+      <Testimonio />  
     </div>
   );
 }
