87342a589749664fba85e24216d721b8d79538ea
Franks Vilca <franksjaviervilcaquispe@gmail.com>
2024-04-11 23:29:27 -0500

    La pagina completamente en blanco

--- src/Componentes/Testimonio.js
+++ src/Componentes/Testimonio.js
@@ -2,10 +2,12 @@ import React from "react";
 
 function Testimonio() {
   return (
+    //El contenedor de cada testimonio
     <div className='contenedor-testimonio'>
       <img 
         className='imagen-testimonio'
-        src='../Imagenes/testimonio-emma.png' />
+        src={require('..\Imagenes\testimonio-emma.png')}
+        alt='Foto de Emma' />
     </div>
   );
 } 
