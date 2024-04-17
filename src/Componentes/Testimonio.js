9e4ae4787e77e2526b7ce500ecc45099f33e5f7e
Franks Vilca <franksjaviervilcaquispe@gmail.com>
2024-04-16 20:15:30 -0500

    Terminado proyecto

--- src/Componentes/Testimonio.js
+++ src/Componentes/Testimonio.js
@@ -10,9 +10,15 @@ function Testimonio(props) {
         src={require(`../imagenes/testimonio-${props.imagen}.png`)}
         alt='Foto de Emma' />
       <div className='contenedor-texto-textimonio'>
-       <p className='nombre-testimonio'>{props.nombre} en {props.pais}</p>  
-       <p className='cargo-testimonio'>{props.cargo} en {props.empresa}</p>
-       <p className='texto-testimonio'>"{props.testimonio}"</p>
+       <p className='nombre-testimonio'>
+          <strong>{props.nombre}</strong> en {props.pais}
+       </p>  
+       <p className='cargo-testimonio'>
+          {props.cargo} en <strong>{props.empresa}</strong>
+       </p>
+       <p className='texto-testimonio'>
+          "{props.testimonio}"
+       </p>
       </div>  
     </div>
   );
